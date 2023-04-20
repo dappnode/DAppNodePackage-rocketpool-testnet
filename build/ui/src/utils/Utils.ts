5b630f85d7cf6854d7da0fb004d2f05e9b9dc96f
Diego <dsimog01@estudiantes.unileon.es>
2023-04-20 16:32:40 +0200

    Fix hardcoded tmp value

--- build/ui/src/utils/Utils.ts
+++ build/ui/src/utils/Utils.ts
@@ -3,37 +3,40 @@ import { NodeStatus } from "../types/NodeStatus";
 
 // export function to convert from wei to ether
 export function toWei(ether: number): number {
-    return ether * 10 ** 18;
+  return ether * 10 ** 18;
 }
 
 export function toEther(wei: number): number {
-    return wei / 10 ** 18;
+  return wei / 10 ** 18;
 }
 
 export function toEtherString(wei: number): string {
-    return toEther(wei).toFixed(4);
+  return toEther(wei).toFixed(4);
 }
 
 export function minipoolUrl(address: string): string {
-    return `https://prater.rocketscan.io/minipool/${address}`;
+  return `https://prater.rocketscan.io/minipool/${address}`;
 }
 
 export function shortenAddress(address: string, chars = 4): string {
-    const prefix = address.slice(0, chars);
-    const suffix = address.slice(-chars);
-    return `${prefix}...${suffix}`;
+  const prefix = address.slice(0, chars);
+  const suffix = address.slice(-chars);
+  return `${prefix}...${suffix}`;
 }
 
 export function isValidEthAddress(address: string): boolean {
-    return /^0x[a-fA-F0-9]{40}$/.test(address);
+  return /^0x[a-fA-F0-9]{40}$/.test(address);
 }
 
-export function enoughEthBalance(nodeStatus?: NodeStatus, networkRplPrice?: NetworkRplPrice) {
-    const minimum8Eth = 8200000000000000000;
-    const minimum16Eth = 16200000000000000000;
-    const minimumRpl8Eth = networkRplPrice?.minPer8EthMinipoolRplStake ?? 0;
-    const ethBalance = 16200000000000000000; // nodeStatus?.accountBalances.eth ?? 0;
-    const rplBalance = 68000000000000000000; // nodeStatus?.accountBalances.rpl ?? 0;
-    const is8EthPool = rplBalance >= minimumRpl8Eth;
-    return is8EthPool ? (ethBalance >= minimum8Eth) : (ethBalance >= minimum16Eth);
-};
+export function enoughEthBalance(
+  nodeStatus?: NodeStatus,
+  networkRplPrice?: NetworkRplPrice
+) {
+  const minimum8Eth = 8200000000000000000;
+  const minimum16Eth = 16200000000000000000;
+  const minimumRpl8Eth = networkRplPrice?.minPer8EthMinipoolRplStake ?? 0;
+  const ethBalance = nodeStatus?.accountBalances.eth ?? 0;
+  const rplBalance = nodeStatus?.accountBalances.rpl ?? 0;
+  const is8EthPool = rplBalance >= minimumRpl8Eth;
+  return is8EthPool ? ethBalance >= minimum8Eth : ethBalance >= minimum16Eth;
+}
