68364b693b72ceb15c514ef260ba1ac5bd7a1d8c
mgarciate <mgarciate@gmail.com>
2023-04-28 11:08:03 +0200

    Fix #42 Add mainnet globals

--- build/rocketpool-start.sh
+++ build/rocketpool-start.sh
@@ -16,43 +16,126 @@ fi
 case $NETWORK in
 "mainnet")
     echo "Mainnet network"
-    _EXECUTION_LAYER_HTTP_GETH="http://geth.dappnode:8545"
-    _EXECUTION_LAYER_WS_GETH="ws://geth.dappnode:8546"
-    _EXECUTION_LAYER_HTTP_NETHERMIND="http://nethermind.dappnode:8545"
-    _EXECUTION_LAYER_WS_NETHERMIND="ws://nethermind.dappnode:8546"
-    _EXECUTION_LAYER_HTTP_BESU="http://besu.dappnode:8545"
-    _EXECUTION_LAYER_WS_BESU="ws://besu.dappnode:8546"
 
-    _BEACON_NODE_API_3500_PRYSM="http://beacon-chain.prysm.dappnode:3500"
-    _BEACON_NODE_API_4000_PRYSM="http://beacon-chain.prysm.dappnode:4000"
-    _BEACON_NODE_API_3500_TEKU="http://beacon-chain.teku.dappnode:3500"
-    _BEACON_NODE_API_4000_TEKU="http://beacon-chain.teku.dappnode:4000"
-    _BEACON_NODE_API_3500_LIGHTHOUSE="http://beacon-chain.lighthouse.dappnode:3500"
-    _BEACON_NODE_API_4000_LIGHTHOUSE="http://beacon-chain.lighthouse.dappnode:4000"
-    _BEACON_NODE_API_3500_NIMBUS="http://beacon-validator.nimbus.dappnode:4500"
-    _BEACON_NODE_API_4000_NIMBUS="http://beacon-validator.nimbus.dappnode:4500"
-    _BEACON_NODE_API_3500_LODESTAR="http://beacon-chain.lodestar.dappnode:3500"
-    _BEACON_NODE_API_4000_LODESTAR="http://beacon-chain.lodestar.dappnode:4000"
+    # Assign proper value to _DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET.
+    case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET in
+    "geth.dnp.dappnode.eth")
+        _EXECUTION_LAYER_HTTP="http://geth.dappnode:8545"
+        _EXECUTION_LAYER_WS="ws://geth.dappnode:8546"
+        ;;
+    "nethermind.dnp.dappnode.eth")
+        _EXECUTION_LAYER_HTTP="http://nethermind.dappnode:8545"
+        _EXECUTION_LAYER_WS="ws://nethermind.dappnode:8546"
+        ;;
+    "besu.dnp.dappnode.eth")
+        _EXECUTION_LAYER_HTTP="http://besu.dappnode:8545"
+        _EXECUTION_LAYER_WS="ws://besu.dappnode:8546"
+        ;;
+    "erigon.dnp.dappnode.eth")
+        _EXECUTION_LAYER_HTTP="http://erigon.dappnode:8545"
+        _EXECUTION_LAYER_WS="ws://erigon.dappnode:8545"
+        ;;
+    *)
+        echo "Unknown value or unsupported for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET Please confirm that the value is correct"
+        exit 1
+        ;;
+    esac
+    
+    # Assign proper value to _DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER.
+    case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_MAINNET" in
+    "prysm.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-chain.prysm.dappnode:3500"
+      _BEACON_NODE_API_4000="http://beacon-chain.prysm.dappnode:4000"
+      _BEACON_NODE_CLIENT="prysm"
+      ;;
+    "teku.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-chain.teku.dappnode:3500"
+      _BEACON_NODE_API_4000="http://beacon-chain.teku.dappnode:4000"
+      _BEACON_NODE_CLIENT="teku"
+      ;;
+    "lighthouse.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-chain.lighthouse.dappnode:3500"
+      _BEACON_NODE_API_4000="http://beacon-chain.lighthouse.dappnode:4000"
+      _BEACON_NODE_CLIENT="lighthouse"
+      ;;
+    "nimbus.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-validator.nimbus.dappnode:4500"
+      _BEACON_NODE_API_4000="http://beacon-validator.nimbus.dappnode:4500"
+      _BEACON_NODE_CLIENT="nimbus"
+      ;;
+    "lodestar.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-chain.lodestar.dappnode:3500"
+      _BEACON_NODE_API_4000="http://beacon-chain.lodestar.dappnode:4000"
+      _BEACON_NODE_CLIENT="lodestar"
+      ;;
+    *)
+      echo "Unknown value or unsupported for _DAPPNODE_GLOBAL_CONSENSUS_CLIENT_MAINNET Please confirm that the value is correct"
+      exit 1
+      ;;
+    esac
+
     ;;
 "prater")
     echo "Prater network"
-    _EXECUTION_LAYER_HTTP_GETH="http://goerli-geth.dappnode:8545"
-    _EXECUTION_LAYER_WS_GETH="ws://goerli-geth.dappnode:8546"
-    _EXECUTION_LAYER_HTTP_NETHERMIND="http://goerli-nethermind.dappnode:8545"
-    _EXECUTION_LAYER_WS_NETHERMIND="ws://goerli-nethermind.dappnode:8546"
-    _EXECUTION_LAYER_HTTP_BESU="http://goerli-besu.dappnode:8545"
-    _EXECUTION_LAYER_WS_BESU="ws://goerli-besu.dappnode:8546"
 
-    _BEACON_NODE_API_3500_PRYSM="http://beacon-chain.prysm-prater.dappnode:3500"
-    _BEACON_NODE_API_4000_PRYSM="http://beacon-chain.prysm-prater.dappnode:4000"
-    _BEACON_NODE_API_3500_TEKU="http://beacon-chain.teku-prater.dappnode:3500"
-    _BEACON_NODE_API_4000_TEKU="http://beacon-chain.teku-prater.dappnode:4000"
-    _BEACON_NODE_API_3500_LIGHTHOUSE="http://beacon-chain.lighthouse-prater.dappnode:3500"
-    _BEACON_NODE_API_4000_LIGHTHOUSE="http://beacon-chain.lighthouse-prater.dappnode:4000"
-    _BEACON_NODE_API_3500_NIMBUS="http://beacon-validator.nimbus-prater.dappnode:4500"
-    _BEACON_NODE_API_4000_NIMBUS="http://beacon-validator.nimbus-prater.dappnode:4500"
-    _BEACON_NODE_API_3500_LODESTAR="http://beacon-chain.lodestar-prater.dappnode:3500"
-    _BEACON_NODE_API_4000_LODESTAR="http://beacon-chain.lodestar-prater.dappnode:4000"
+    # https://github.com/dappnode/DAppNodePackage-SSV-Shifu/blob/775dfbc2190b8c3bc7384a2e4c62d83892071001/build/entrypoint.sh#L3
+    # Assign proper value to _DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER.
+    case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER in
+    "goerli-geth.dnp.dappnode.eth")
+        _EXECUTION_LAYER_HTTP="http://goerli-geth.dappnode:8545"
+        _EXECUTION_LAYER_WS="ws://goerli-geth.dappnode:8546"
+        ;;
+    "goerli-nethermind.dnp.dappnode.eth")
+        _EXECUTION_LAYER_HTTP="http://goerli-nethermind.dappnode:8545"
+        _EXECUTION_LAYER_WS="ws://goerli-nethermind.dappnode:8546"
+        ;;
+    "goerli-besu.dnp.dappnode.eth")
+        _EXECUTION_LAYER_HTTP="http://goerli-besu.dappnode:8545"
+        _EXECUTION_LAYER_WS="ws://goerli-besu.dappnode:8546"
+        ;;
+    "goerli-erigon.dnp.dappnode.eth")
+        _EXECUTION_LAYER_HTTP="http://goerli-erigon.dappnode:8545"
+        _EXECUTION_LAYER_WS="ws://goerli-erigon.dappnode:8545"
+        ;;
+    *)
+        echo "Unknown value or unsupported for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER Please confirm that the value is correct"
+        exit 1
+        ;;
+    esac
+
+    # Assign proper value to _DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER.
+    case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER" in
+    "prysm-prater.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-chain.prysm-prater.dappnode:3500"
+      _BEACON_NODE_API_4000="http://beacon-chain.prysm-prater.dappnode:4000"
+      _BEACON_NODE_CLIENT="prysm"
+      ;;
+    "teku-prater.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-chain.teku-prater.dappnode:3500"
+      _BEACON_NODE_API_4000="http://beacon-chain.teku-prater.dappnode:4000"
+      _BEACON_NODE_CLIENT="teku"
+      ;;
+    "lighthouse-prater.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-chain.lighthouse-prater.dappnode:3500"
+      _BEACON_NODE_API_4000="http://beacon-chain.lighthouse-prater.dappnode:4000"
+      _BEACON_NODE_CLIENT="lighthouse"
+      ;;
+    "nimbus-prater.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-validator.nimbus-prater.dappnode:4500"
+      _BEACON_NODE_API_4000="http://beacon-validator.nimbus-prater.dappnode:4500"
+      _BEACON_NODE_CLIENT="nimbus"
+      ;;
+    "lodestar-prater.dnp.dappnode.eth")
+      _BEACON_NODE_API_3500="http://beacon-chain.lodestar-prater.dappnode:3500"
+      _BEACON_NODE_API_4000="http://beacon-chain.lodestar-prater.dappnode:4000"
+      _BEACON_NODE_CLIENT="lodestar"
+      ;;
+    *)
+      echo "Unknown value or unsupported for _DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER Please confirm that the value is correct"
+      exit 1
+      ;;
+    esac
+
     ;;
 *)
     echo "Unknown value or unsupported for NETWORK Please confirm that the value is correct"
@@ -60,62 +143,8 @@ case $NETWORK in
     ;;
 esac
 
-# https://github.com/dappnode/DAppNodePackage-SSV-Shifu/blob/775dfbc2190b8c3bc7384a2e4c62d83892071001/build/entrypoint.sh#L3
-# Assign proper value to _DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER.
-case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER in
-"goerli-geth.dnp.dappnode.eth")
-    _EXECUTION_LAYER_HTTP=$_EXECUTION_LAYER_HTTP_GETH
-    _EXECUTION_LAYER_WS=$_EXECUTION_LAYER_WS_GETH
-    ;;
-"goerli-nethermind.dnp.dappnode.eth")
-    _EXECUTION_LAYER_HTTP=$_EXECUTION_LAYER_HTTP_NETHERMIND
-    _EXECUTION_LAYER_WS=$_EXECUTION_LAYER_WS_NETHERMIND
-    ;;
-"goerli-besu.dnp.dappnode.eth")
-    _EXECUTION_LAYER_HTTP=$_EXECUTION_LAYER_HTTP_BESU
-    _EXECUTION_LAYER_WS=$_EXECUTION_LAYER_WS_BESU
-    ;;
-*)
-    echo "Unknown value or unsupported for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER Please confirm that the value is correct"
-    exit 1
-    ;;
-esac
-
 export EXECUTION_LAYER_HTTP=$_EXECUTION_LAYER_HTTP
 export EXECUTION_LAYER_WS=$_EXECUTION_LAYER_WS
-
-# Assign proper value to _DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER.
-case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER" in
-"prysm-prater.dnp.dappnode.eth")
-  _BEACON_NODE_API_3500=$_BEACON_NODE_API_3500_PRYSM
-  _BEACON_NODE_API_4000=$_BEACON_NODE_API_4000_PRYSM
-  _BEACON_NODE_CLIENT="prysm"
-  ;;
-"teku-prater.dnp.dappnode.eth")
-  _BEACON_NODE_API_3500=$_BEACON_NODE_API_3500_TEKU
-  _BEACON_NODE_API_4000=$_BEACON_NODE_API_4000_TEKU
-  _BEACON_NODE_CLIENT="teku"
-  ;;
-"lighthouse-prater.dnp.dappnode.eth")
-  _BEACON_NODE_API_3500=$_BEACON_NODE_API_3500_LIGHTHOUSE
-  _BEACON_NODE_API_4000=$_BEACON_NODE_API_4000_LIGHTHOUSE
-  _BEACON_NODE_CLIENT="lighthouse"
-  ;;
-"nimbus-prater.dnp.dappnode.eth")
-  _BEACON_NODE_API_3500=$_BEACON_NODE_API_3500_NIMBUS
-  _BEACON_NODE_API_4000=$_BEACON_NODE_API_4000_NIMBUS
-  _BEACON_NODE_CLIENT="nimbus"
-  ;;
-"lodestar-prater.dnp.dappnode.eth")
-  _BEACON_NODE_API_3500=$_BEACON_NODE_API_3500_LODESTAR
-  _BEACON_NODE_API_4000=$_BEACON_NODE_API_4000_LODESTAR
-  _BEACON_NODE_CLIENT="lodestar"
-  ;;
-*)
-  echo "Unknown value or unsupported for _DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER Please confirm that the value is correct"
-  exit 1
-  ;;
-esac
 
 export BEACON_NODE_API_3500=$_BEACON_NODE_API_3500
 export BEACON_NODE_API_4000=$_BEACON_NODE_API_4000
